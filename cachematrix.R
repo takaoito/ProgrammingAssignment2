9726954d7a1a4f2442a54ded6ca64773bf2333c8
Takao Ito <takaoito.itb@gmail.com>
2016-04-25 20:39:42 +0900

    created assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,32 @@
 ## R programming assignemnt 2
-
 ## Matrix Inversion
 
-makeCacheMatrix <- function(x = matrix()) {
+## cash matrix
 
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    y <<- x
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set=set, get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## calculate inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
